d4ee75ad70666869b81514828dbd5f6fcce88efd
virchau13 <computernerd7777@gmail.com>
2019-03-02 16:35:26 +0800

    added some features to protector

--- protector-app/App.js
+++ protector-app/App.js
@@ -3,19 +3,10 @@ import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 
 const colors = {
     bg_green: "rgb(148,199,138)",
+    bg_red: "rgb(196,124,122)",
     primary: "#4caf50",
     secondary: "#ab47bc"
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.bg_green,
-        paddingLeft: Dimensions.get('window').width/10,
-        justifyContent: 'center',
-        paddingBottom: Dimensions.get('window').width/4
-    },
-});
 
 
 function sectotime(t){
@@ -36,21 +27,29 @@ export default class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            time: 5
+            time: 5,
+            bg: colors.bg_green,
+            trusted: true
         }
         this.startTimer();
     }
     startTimer(){
         this.timer = setInterval(()=>{
-            this.setState({time: this.state.time-1});
+            this.setState({
+                time: this.state.time-1,
+                bg: (this.state.time-1 <= 0 ? colors.bg_red : colors.bg_green),
+                trusted: true
+            });
         }, 1000);
+        
     }
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={{fontWeight: "bold", fontSize: Dimensions.get('window').height/20, margin: 0, opacity: 0.7}}>You're safe</Text>
-                <Text style={{fontSize: Dimensions.get('window').height/15, marginTop: -10, opacity: 0.7}}>{sectotime(this.state.time)}</Text>
-                <View style={{width: Dimensions.get('window').width/2, paddingTop: 20}}><Button title="I'm here, reset timer" color={colors.primary} onPress={()=>{}}></Button></View>
+            <View style={{flex: 1, backgroundColor: this.state.bg, paddingLeft: Dimensions.get('window').width/10, justifyContent: 'center', paddingBottom: Dimensions.get('window').width/4}}>
+                <Text style={{fontSize: 20, opacity: 0.7}}>Time left:</Text>
+                <Text style={{fontSize: Dimensions.get('window').height/20, marginTop: -10, opacity: 0.7}}>{sectotime(this.state.time)}</Text>
+                <Text style={{fontSize: Dimensions.get('window').height/20, marginTop: -10, opacity: 0.7}}>{(this.state.trusted ? "Trusted" : "Untrusted") + " location"}</Text>
+                <View style={{width: Dimensions.get('window').width/2, paddingTop: 20}}><Button title="Contact" color={colors.secondary} onPress={()=>{}}></Button></View>
             </View>
         );
     }
