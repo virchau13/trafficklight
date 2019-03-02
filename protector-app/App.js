be1c81dad99415c2d0f79f468d0828616addfe9f
smjleo <leolsj1023@gmail.com>
2019-03-02 16:46:47 +0800

    fixed protector

--- protector-app/App.js
+++ protector-app/App.js
@@ -5,7 +5,7 @@ const colors = {
     bg_green: "rgb(148,199,138)",
     bg_red: "rgb(196,124,122)",
     primary: "#4caf50",
-    secondary: "#ab47bc"
+    secondary: "#4caf50"
 }
 
 
@@ -41,15 +41,15 @@ export default class App extends React.Component {
                 trusted: true
             });
         }, 1000);
-        
+
     }
     render() {
         return (
             <View style={{flex: 1, backgroundColor: this.state.bg, paddingLeft: Dimensions.get('window').width/10, justifyContent: 'center', paddingBottom: Dimensions.get('window').width/4}}>
-                <Text style={{fontSize: 20, opacity: 0.7}}>Time left:</Text>
-                <Text style={{fontSize: Dimensions.get('window').height/20, marginTop: -10, opacity: 0.7}}>{sectotime(this.state.time)}</Text>
-                <Text style={{fontSize: Dimensions.get('window').height/20, marginTop: -10, opacity: 0.7}}>{(this.state.trusted ? "Trusted" : "Untrusted") + " location"}</Text>
-                <View style={{width: Dimensions.get('window').width/2, paddingTop: 20}}><Button title="Contact" color={colors.secondary} onPress={()=>{}}></Button></View>
+                <Text style={{fontSize: 20, opacity: 0.7, paddingBottom: 10}}>Last seen:</Text>
+                <Text style={{fontSize: Dimensions.get('window').height/20, marginTop: -10, opacity: 0.7, fontWeight: "bold"}}>{sectotime(this.state.time)}</Text>
+                <Text style={{fontSize: Dimensions.get('window').height/20, marginTop: -10, opacity: 0.7, fontWeight: "bold"}}>{(this.state.trusted ? "Trusted" : "Untrusted") + " location"}</Text>
+                <View style={{width: Dimensions.get('window').width/3, paddingTop: 20}}><Button title="Contact" color={colors.secondary} onPress={()=>{}}></Button></View>
             </View>
         );
     }
